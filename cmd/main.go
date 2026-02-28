b116b19b58476c76866cb911ee6fb8b57915fe1e
Chung-Hsuan Tsai <zx.c@nycu.edu.tw>
2026-02-28 15:56:23 +0800

    fix: update LeaderElectionID for consistency in main.go
    - Changed the LeaderElectionID from "otterscale-template-leader-election" to "operator-template-leader-election" for improved clarity and alignment with naming conventions.

--- cmd/main.go
+++ cmd/main.go
@@ -159,7 +159,7 @@ func main() {
 		WebhookServer:          webhookServer,
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "otterscale-template-leader-election",
+		LeaderElectionID:       "operator-template-leader-election",
 		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
 		// when the Manager ends. This requires the binary to immediately end when the
 		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
